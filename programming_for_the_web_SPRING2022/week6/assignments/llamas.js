66fe865db832141569a2a568033a3823807fc972
Erin Kaufman <ekaufman@mcad.edu>
2022-02-27 16:18:59 -0600

    worked on week 6 assignment

---
+++ programming_for_the_web_SPRING2022/week6/assignments/llamas.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: "#myInfo",
+    data: {
+      llamaFriend: "Moth",
+      llamaColor: "Aqua",
+      llamaSnack: "Ruffles",
+      llamaCar: "Dodge Neon",
+      llamaTrip: "Canada",
+      llamaBand: "Led Zeppelin",
+      llamaDrug: "Tylenol",
+      llamaStore: "JC Penney",
+      llamaShirt: "T-shirt",
+      llamaCurse: "Dammit"
+    }
+  })
