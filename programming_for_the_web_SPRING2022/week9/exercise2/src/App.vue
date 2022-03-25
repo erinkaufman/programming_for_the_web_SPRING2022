dae93a1990f93a4c7b1d07a20d9c5873f7e0ba78
Erin Kaufman <ekaufman@mcad.edu>
2022-03-24 19:40:13 -0500

    work on week 9 exercise 2

---
+++ programming_for_the_web_SPRING2022/week9/exercise2/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import CamelidRow from "../src/components/CamelidRow.vue";
+const camelids = [
+      {
+        commonName: "Llama",
+        binomialName: "Llama glama",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Llama",
+        species: "L. glama",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg"
+        },
+      {
+        commonName: "Alpaca",
+        binomialName: "Vicugna pacos",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Vicugna",
+        species: "V. pacos",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg"
+      },
+    ]
+</script>
+
+<template>
+<div>
+<h1>This will be my Camelids page</h1>
+  <div class="camelid-container">
+    <CamelidRow v-for="critter in camelids" v-bind:key="critter.commonName" v-bind:animal="critter" />
+  </div>
+</div>
+</template>
+
+<style>
+
+body {
+  font-family: 'Courier New', Courier, monospace;
+}
+
+.camelid-container {
+  display: flex;
+  flex-direction: row;
+}
+
+</style>
